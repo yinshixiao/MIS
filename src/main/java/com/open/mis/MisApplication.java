bee1d8f8db5961439f405db50ce948752041d864
yinshixiao <593853565@qq.com>
2018-06-20 21:31:05 +0800

    1

---
+++ src/main/java/com/open/mis/MisApplication.java
@@ -0,0 +1,12 @@
+package com.open.mis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisApplication.class, args);
+	}
+}
