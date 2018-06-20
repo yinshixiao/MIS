98660178597cf7a5f449ce50b9cde320db31f130
yinshixiao <593853565@qq.com>
2018-06-20 21:59:00 +0800

    1

---
+++ src/main/java/com/open/mis/config/dao/DataSourceConfiguration.java
@@ -0,0 +1,38 @@
+package com.open.mis.config.dao;
+
+import java.beans.PropertyVetoException;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+
+
+@Configuration
+@MapperScan("com.viewhigh.path.**")
+public class DataSourceConfiguration {
+	
+	@Value("${jdbc.driver}")
+    private String driver;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+   
+    
+    @Bean(name="dataSource")
+    public ComboPooledDataSource dataSource() throws PropertyVetoException{
+    	ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass(driver);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+        dataSource.setAutoCommitOnClose(false);
+        return dataSource;
+    }
+    
+}
