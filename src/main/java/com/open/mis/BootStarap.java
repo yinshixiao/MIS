396526b541a2e56e8788e4cffff4e99a93bb260c
yinshixiao <593853565@qq.com>
2018-08-06 21:37:55 +0800

    1111111

--- src/main/java/com/open/mis/BootStarap.java
+++ src/main/java/com/open/mis/BootStarap.java
@@ -2,11 +2,11 @@ package com.open.mis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class BootStarap {
    
 	public static void main(String[] args) {
 		SpringApplication.run(BootStarap.class, args);
 	}
-}
+} 
