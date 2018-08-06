0ac94c907a94fe9c4e7da54a1b323a77a528fcbf
yinshixiao <593853565@qq.com>
2018-08-06 21:35:23 +0800

    ````

--- src/main/java/com/open/mis/BootStarap.java
+++ src/main/java/com/open/mis/BootStarap.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootStarap {
-
+   
 	public static void main(String[] args) {
 		SpringApplication.run(BootStarap.class, args);
 	}
